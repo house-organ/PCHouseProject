4600a41c61c0a7bdd0c2bf37629f8ba40fc340f2
zhixi0912 <382893000@qq.com>
2019-06-06 17:51:20 +0800

    nav

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import { Row, Col ,Menu, Icon,Input } from 'antd';
+import { Row, Col ,Menu, Icon,Input,Breadcrumb  } from 'antd';
 import "./index.scss";
 
 
@@ -11,6 +11,7 @@ const Search = Input.Search;
 export default class Nav extends Component {
     state={
         menuList:[
+            {id:'0',key:'city',name:'海南'},
             {id:'1',key:'index',name:'首页'},
             {id:'2',key:'newRoom',name:'新房'},
             {id:'3',key:'seaViewRoom',name:'海景房'},
@@ -23,26 +24,53 @@ export default class Nav extends Component {
     }
     render() {
         const menu = this.state.menuList.map(item=>{
-            return (<Menu.Item key={item.id}><a href={"/"+(item.key == 'index' ? ' ':item.key)}>{item.name}</a></Menu.Item>)
+            if(item.key==='city'){
+                return (<Menu.Item className="city-li" key={item.id}><Icon type="bank" /><a href={"/"+(item.key == 'index' ? ' ':item.key)}>{item.name}</a><Icon className="maen-icon" type="down" /></Menu.Item>)
+            }else {
+                return (<Menu.Item key={item.id}><a href={"/"+(item.key == 'index' ? ' ':item.key)}>{item.name}</a></Menu.Item>)
+            }
+
         })
         return (
             <div className="header">
-                <Row type="flex" justify="start" className="header-box">
-                    <Col className="logo-box"><img className="logo" src="src/public/images/logo.png" /></Col>
-                    <Col span={12}>
-                        <Menu onClick={this.handleClick} mode="horizontal" className="header-menu">
-                            {menu}
-                        </Menu>
-                    </Col>
-                    <Col className="search-box">
-                        <Search
-                            placeholder="input search text"
+                <div  className="header-top">
+                    <Row type="flex" justify="start" className="header-box">
+                        <Col className="logo-box"><img className="logo" src="src/public/images/logo.png" /></Col>
+                        <Col className="header-menu-box">
+                            <Menu onClick={this.handleClick} mode="horizontal" className="header-menu">
+                                {menu}
+                            </Menu>
+                        </Col>
+                        <Col className="search-box">
+                            <Search
+                                placeholder="请输入楼盘名称"
 
-                            onSearch={value => console.log(value)}
-                        />
-                    </Col>
-                    <Col className="tel-box"><img src="src/public/images/tel-icon-1.png"  />123456789</Col>
-                </Row>
+                                onSearch={value => console.log(value)}
+                            />
+                        </Col>
+                        <Col className="tel-box">
+                            <div className="tel-box-1">
+                                <div className="tel-title-1"><img src="src/public/images/tel-icon-1.png"  />咨询热线</div>
+                                <div className="tel-title-2">123456789</div>
+                            </div>
+                            <div className="tel-box-2" style={{display:'none'}}>
+                                <img src="src/public/images/tel-icon-1.png"  />0123-123456789
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div  className="breadcrumb">
+                    <div  className="breadcrumb-box">
+                        <Breadcrumb separator=">">
+                            <Breadcrumb.Item>您的位置：</Breadcrumb.Item>
+                            <Breadcrumb.Item>首页</Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <a href="">楼盘信息</a>
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                    </div>
+                </div>
+
             </div>
         );
     }
